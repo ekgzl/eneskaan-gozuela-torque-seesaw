cd497b5127f81a3bd610eedaea621938342a2ceb
Enes <enesgozuela@gmail.com>
2025-10-22 12:44:52 +0300

    Torque handling in main.js; update distance calculations and angle display on click events. Also, plank is rotating now without animation. Unit of measurement is added into html.

--- src/js/main.js
+++ src/js/main.js
@@ -10,17 +10,25 @@ const insideScreen = document.getElementById("inside-screen");
 
 nextWeightDisplay.innerHTML = generateRandomWeight();
 
-// TODO: put all objects into array.
-// let objects = [];
+let objects = [];
+let rightTorque = 0;
+let leftTorque = 0;
 
 function addWeightToSide(side, weight) {
-  console.log(weight, "gelen", side);
   if (side === "right") {
     rightWeightDisplay.innerHTML =
       parseInt(rightWeightDisplay.textContent) + weight;
   } else {
     leftWeightDisplay.innerHTML =
-      parseInt(rightWeightDisplay.textContent) + weight;
+      parseInt(leftWeightDisplay.textContent) + weight;
+  }
+}
+
+function addTorqueToSide(side, weight, distance) {
+  if (side === "right") {
+    rightTorque += calculateTorque(distance, weight);
+  } else {
+    leftTorque += calculateTorque(distance, weight);
   }
 }
 
@@ -30,24 +38,39 @@ function generateRandomWeight() {
 
 function handleClick(side, distance) {
   const weight = parseInt(nextWeightDisplay.textContent);
+  objects.push({
+    weight,
+    side,
+    distance,
+  });
   addWeightToSide(side, weight);
+  addTorqueToSide(side, weight, distance);
   let randomWeight = generateRandomWeight();
   nextWeightDisplay.innerHTML = randomWeight;
 }
 
 function calculateTorque(distance, weight) {
-  return distance * weight;
+  return distance * weight * 9.8;
+}
+
+function calculateAngle() {
+  return Math.max(-30, Math.min(30, (rightTorque - leftTorque) / 10));
 }
 
 insideScreen.addEventListener("click", function (event) {
   // event.offsetX div'in sol kenarına uzaklığı pixel olarak veriyor
   // this.offsetWidth div'in toplam genişliğini veriyor
-  console.log("2 kere");
   const centerPoint = this.offsetWidth / 2;
   const distance = event.offsetX;
+
   if (distance < centerPoint) {
-    handleClick("left", centerPoint - distance);
+    const realDistance = (centerPoint - distance) / 50;
+    handleClick("left", realDistance);
   } else {
-    handleClick("right", distance - centerPoint);
+    const realDistance = (distance - centerPoint) / 50;
+    handleClick("right", realDistance);
   }
+
+  angleDisplay.innerHTML = calculateAngle().toFixed(2);
+  plank.style.transform = `rotate(${parseInt(angleDisplay.textContent)}deg)`;
 });
